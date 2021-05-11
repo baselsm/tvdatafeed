b8f8cf181862ba0542b03028cc9bb371ebd5cf8b
StreamAlpha <59556194+StreamAlpha@users.noreply.github.com>
2021-05-11 20:05:28 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name='tvdatafeed',
-    version='1.0.0',
+    version='1.0.1',
     packages=['tvDatafeed'],
-    url='https://www.youtube.com/channel/UC-YF3S7qFB2mj_B78DKPCHw',
+    url='https://www.youtube.com/watch?v=qDrXmb2ZRjo',
     license='MIT License',
     author='@anojangra',
     author_email='',
