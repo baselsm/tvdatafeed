78bb967bbc1b64fcd18b651044cdf2d00c648b3b
StreamAlpha <59556194+StreamAlpha@users.noreply.github.com>
2021-05-11 20:03:12 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='tvdatafeed',
+    version='1.0.0',
+    packages=['tvDatafeed'],
+    url='https://www.youtube.com/channel/UC-YF3S7qFB2mj_B78DKPCHw',
+    license='MIT License',
+    author='@anojangra',
+    author_email='',
+    description='TradingView historical data downloader'
+)
