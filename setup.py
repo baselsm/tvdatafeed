24a17af6125b8f90ce78aad88c87b873ddb5fe70
Anoop Jangra <59556194+anoopjangra@users.noreply.github.com>
2021-07-03 22:29:05 +0530

    -

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
-    name='tvdatafeed',
-    version='1.0.4',
-    packages=['tvDatafeed'],
-    url='https://www.youtube.com/watch?v=qDrXmb2ZRjo',
-    license='MIT License',
-    author='@StreamAlpha',
-    author_email='',
-    description='TradingView historical data downloader'
+    name="tvdatafeed",
+    version="1.0.4",
+    packages=["tvDatafeed"],
+    url="https://www.youtube.com/watch?v=qDrXmb2ZRjo",
+    license="MIT License",
+    author="@StreamAlpha",
+    author_email="",
+    description="TradingView historical data downloader",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
 )
